127e86689f0bd3195e2a92130a8007f281cf22fe
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2024-03-01 11:02:23 -0800

    Tag created resources

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
 resource "aws_iam_policy" "profile" {
   name_prefix = var.profile_name
   policy      = var.permissions
+  tags        = local.tags
 }
 
 resource "aws_iam_role" "profile" {
   name_prefix        = var.profile_name
   assume_role_policy = data.aws_iam_policy_document.assume.json
+  tags               = local.tags
 }
 
 resource "aws_iam_role_policy_attachment" "profile" {
@@ -16,6 +18,7 @@ resource "aws_iam_role_policy_attachment" "profile" {
 resource "aws_iam_instance_profile" "profile" {
   name = var.profile_name
   role = aws_iam_role.profile.name
+  tags = local.tags
 }
 
 resource "aws_iam_role_policy_attachment" "extra" {
