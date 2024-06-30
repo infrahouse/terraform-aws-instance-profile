3de43518cd65a4c65a5a09e070bdf0abc747cc6d
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2024-06-29 19:57:59 -0700

    Export role policy name/arn as outputs

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,14 @@ output "instance_role_arn" {
   description = "Role ARN that the instance gets."
   value       = aws_iam_role.profile.arn
 }
+
+
+output "instance_role_policy_name" {
+  description = "Role policy name that the instance gets."
+  value       = aws_iam_policy.profile.name
+}
+
+output "instance_role_policy_arn" {
+  description = "Role policy ARN that the instance gets."
+  value       = aws_iam_policy.profile.arn
+}
