6b702d1ef62205d53ca0e3e2dc0a6ae29b76d1b7
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2025-01-19 16:31:19 -0800

    Tags created_by_module and upstream_module must take recedence

--- locals.tf
+++ locals.tf
@@ -1,9 +1,9 @@
 locals {
   tags = merge(
+    var.tags,
     {
       created_by_module : "infrahouse/instance-profile/aws"
       upstream_module : var.upstream_module
-    },
-    var.tags
+    }
   )
 }
